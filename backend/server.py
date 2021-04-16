ff0873f236726cf38fda5dffac00563e5424ba7b
hedy <2862514060@qq.com>
2021-04-16 15:38:43 +0800

    后端api开发1-作业

---
+++ backend/server.py
@@ -0,0 +1,33 @@
+from flask import Flask, request
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+app.config['testcase'] = []
+
+
+class TestCase(Resource):
+    def get(self):
+        print(request.args)
+        if "id" in request.args:
+            for item in app.config['testcase']:
+                if request.args['id'] == str(item['id']):
+                    return item
+        else:
+            return app.config['testcase']
+
+    def post(self):
+        """
+        上传测试用例：id name description steps
+
+        :return:
+        """
+        app.config['testcase'].append(request.json)
+        print(app.config['testcase'])
+        return "用例添加成功"
+
+
+api.add_resource(TestCase, '/testcase')
+
+if __name__ == "__main__":
+    app.run(debug=1)
